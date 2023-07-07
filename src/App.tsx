0cdacd2fc61eede02997cd47a3c20a26bdd4b245
lucasnather <natherlucas@gmail.com>
2023-07-07 12:24:29 -0400

    feat: estilos globais e tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Route/router'
+import { GlobalStyle } from './globalStyle'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './theme/defaultTheme'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
